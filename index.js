113bdf3b853f845770237c1d27b28a517dd15569
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-04-23 18:56:47 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+<html>
+<head>
+	<title>AppleScript Highlighter</title>
+</head>
+<body>
+
+<link rel="stylesheet" 
+	   href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@latest/build/styles/default.min.css">
+<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@latest/build/highlight.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@latest/build/languages/applescript.min.js"></script>
+
+<style>
+	.splitScreen {
+		display: flex;
+		flex-direction: row;
+	}
+	.halfScreen {
+		height: 80vh;
+		overflow: scroll;
+		margin: auto;
+	}
+	.halfScreen-left {
+		width: 40vw;
+	}
+	.halfScreen-right {
+		width: 59vw;
+	}
+	.input {
+		background-color: black;
+		color: white;
+	}
+</style>
+
+<h1>Basic AppleScript Highlighter</h1>
+<div class="splitScreen">
+	<textarea id="codeInput" class="halfScreen input halfScreen-left" placeholder="write code here..." ></textarea>
+	<pre class="halfScreen halfScreen-right">
+		<code id="codeArea" class="applescript"></code>
+	</pre>
+</div>
+
+<script>
+	addEventListener('load', () => {
+		codeInput.onkeyup = update;
+		codeInput.onchange = update;
+		update();
+	});
+	
+	function update(){
+		codeArea.innerHTML = codeInput.value;
+		highlightApplescript();
+	}
+	
+	function highlightApplescript(){
+		hljs.highlightBlock(codeArea);
+	}
+</script>
+
+</body>
+</html>
